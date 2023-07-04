a22f95efadaf3053d712aedc4bb6644070aec5a6
vinay8462 <138144751+vinay8462@users.noreply.github.com>
2023-07-04 18:33:21 +0530

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 
-const databasePath = path.join(__dirname, "covid19India.db");
+const databasePath = path.join(__dirname, "cricketMatchDetails.db");
 
 const app = express();
 
